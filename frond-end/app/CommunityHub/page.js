9ed93f2d9a1c48971ff81e81e9af7c059794a90c
CheniIyes <cheniiyes@gmail.com>
2024-02-15 15:22:06 +0100

    f

--- frond-end/app/CommunityHub/page.js
+++ frond-end/app/CommunityHub/page.js
@@ -1,16 +1,35 @@
 'use client'
 
-
+import { AiOutlineSearch } from "react-icons/ai";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './CommunityHub.css'
 
 const Community = () => {
       const [postes, setPostes] = useState([]);
-
+      const [searchTerm, setSearchTerm] = useState('');
       useEffect(() => {
             getAllPostes();
       }, []);
+      const handleSearchTermChange = (event) => {
+            setSearchTerm(event.target.value);
+      };
+
+      const handleSearchButtonClick = () => {
+            fetchNewsByTitle();
+      };
+      const fetchNewsByTitle = async () => {
+            try {
+                  const response = await axios.get(`http://localhost:4000/postes/search?title=${searchTerm}`);
+                  console.log('Search Response:', response.data);
+
+                  if (Array.isArray(response.data)) {
+                        setPostes(response.data);
+                  }
+            } catch (error) {
+                  console.error('Error searching jaw:', error);
+            }
+      };
 
       const getAllPostes = async () => {
             try {
@@ -26,13 +45,34 @@ const Community = () => {
       };
 
       return (
-            <div className="community-container">
-                  {postes.map((post) => (
-                        <div key={post.id} className="community-card">
-                              <p className="community-title">{post.title}</p>
-                              <p className="community-description">{post.description}</p>
+            <div>
+                  <div className='search'>
+                        <div className="postes">
+                              <div className="FIND-HUBS">FIND HUBS</div>
+                              <input
+                                    className='searchh-inputt'
+                                    type="text"
+                                    placeholder="Search for something..."
+                                    value={searchTerm}
+                                    onChange={handleSearchTermChange}
+                              />
+                              <AiOutlineSearch className="searchh-buttonn" onClick={handleSearchButtonClick} />
                         </div>
-                  ))}
+                  </div>
+                  <div className="community-container">
+
+                        {postes.map((post) => (
+                              <div key={post.id} className="community-card">
+                                    <div className="community-title">
+                                          <hr />
+                                          {post.title}
+                                    </div>
+                                    <div className="community-description">
+                                          {post.description}
+                                    </div>
+                              </div>
+                        ))}
+                  </div>
             </div>
       );
 };
