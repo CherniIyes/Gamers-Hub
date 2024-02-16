039db16f7e7c947783d0a44ef94b03d8db625d76
guelmamihiba <guelmamihiba1@gmail.com>
2024-02-16 14:13:38 +0100

    again

--- back-end/Routes/LatestGamesRoutes.js
+++ back-end/Routes/LatestGamesRoutes.js
@@ -3,9 +3,9 @@ const express = require("express");
 const router = express.Router();
 const LatestGamesController = require("../Controllers/LatestGamesController");
 
-router.get("/", LatestGamesController.getAllLatestGames);
+router.get("/getAll", LatestGamesController.getAllLatestGames);
 router.get("/:id", LatestGamesController.getLatestGameById);
-router.post("/", LatestGamesController.createLatestGame);
+router.post("/add", LatestGamesController.createLatestGame);
 router.put("/:id", LatestGamesController.updateLatestGame);
 router.delete("/:id", LatestGamesController.deleteLatestGame);
 
